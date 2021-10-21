300fad127c21d87aeb1e681d8d3590e8d2daf511
Shizhong-Shang <ShizhongShang@cmail.carleton.ca>
2021-10-20 21:53:53 -0400

    create player.java: Square list, player's name, player's color

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,40 +1,79 @@
 package Model;
+import java.awt.*;
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
+    private List<Square> squaresOwn; // the player's owned squares
+    private String name; // the player's name
+    private Color color;
 
-    public Player(String name, int i) {
+    /**
+     * Constructor of Player
+     * make an array list for the owner's squares called squaresOwn
+     */
+    public Player(String name) {
+        this.name = name;
+        this.squaresOwn = new ArrayList<>();
     }
 
-    void setLocation(Square location){}
-
-    public Square getLocation() {
-        return null;
+    /**
+     * @ param square is added into the squaresOwn array list
+     */
+    public void addSquare(Square square) {
+        this.squaresOwn.add(square);
+    }
+    public void addColor(Color c)
+    {
+        color = c;
     }
 
-    public boolean buyProperty(Square location) {
-        return true;
+    public Color getColor() {
+        return color;
     }
 
-    public int getCash() {
-        return 0;
+    /**
+     * @ param square is removed from the squaresOwn array list
+     */
+    public void removeSquare(Square square) {
+        this.squaresOwn.remove(square);
     }
 
-    public boolean isBankrupt() {
-        return true;
+    /**
+     * get all the countries names from the squareOwn array list
+     * @ return a list of the owner's squares
+     */
+    public String getStatus() {
+        String s = "";
+        s += "Player: " + this.name + " has squares:\n";
+        for (Square square : squaresOwn) {
+            s += "  " + square.printState() + "\n";
+        }
+        return s + "\n";
     }
 
+    /**
+     * @ return squaresOwn array list
+     */
+    public List<Square> getSquaresOwn() { // helper method used for getting all the available countries in a printable list for the player
+        return squaresOwn;
+    }
+
+    public String getSquaresInString(){
+        String s="";
+        for(Square c: squaresOwn){
+            s += c.toString();
+        }
+        return s;
+    }
+
+    /**
+     * @ return name of the squares
+     */
     public String getName() {
-        return null;
+        return name;
     }
 
-    public ArrayList<PropertySquare> getProperties() {
-        return null;
-    }
-
-    public void decreaseCash(int rentFee) {
-    }
-
-    public void increaseCash(int rent) {
-    }
+    public void clear(){this.squaresOwn.clear();}
 }
