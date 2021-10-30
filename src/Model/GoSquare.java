476ce07be357271ea8c4eab7ed889ab338f8530f
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2021-10-30 01:07:24 -0400

    GoSquare

---
+++ src/Model/GoSquare.java
@@ -0,0 +1,21 @@
+package Model;
+
+public class GoSquare extends Square{
+    private final int addMoney;
+
+    public GoSquare(String name, int number, int money) {
+        super(name, number);
+        this.addMoney = money;
+    }
+
+    private int getAddMoney() {
+        return addMoney;
+    }
+
+    @Override
+    public void landOn(Player p) {
+        int money = this.getAddMoney();
+        System.out.println("!!!You will get some money!!!");
+        System.out.println(p.getName() + " got $" + money);
+    }
+}
