61caea78c6b74710284eca21fcd94f38720419cc
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2021-11-18 22:25:15 -0500

    Update DiceGUI.java

--- src/view/DiceGUI.java
+++ src/view/DiceGUI.java
@@ -1,31 +1,37 @@
 package view;
 
-import Model.Dice;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+
 
 public class DiceGUI extends JPanel {
 
-    private Dice d = new Dice();
+    public static final int SIZE = 90;
 
+
+    /**
+     * initialize the dice panel
+     */
     public DiceGUI() {
-        JPanel image = new JPanel(new GridLayout(1,2));
-        JPanel full = new JPanel(new BorderLayout());
+        this.setPreferredSize(new Dimension(150, 80));
+        this.setLayout(new BorderLayout());
+    }
 
-        image.setPreferredSize(new Dimension(200,100));
-        full.setPreferredSize(new Dimension(200,150));
+    /**
+     * set dice image according to dice values
+     */
+    public void setDiceImages(int dice1Value, int dice2Value) {
+        String dice1Path = "src/images/Dice" + dice1Value + ".png";
+        String dice2Path = "src/images/Dice" + dice2Value + ".png";
 
-        JLabel totalDiceValue = new JLabel();
-        totalDiceValue.setText("Total Dice Value: " + d.getTotalValue());
+        ImageIcon dice1Image = new ImageIcon(dice1Path);
+        ImageIcon dice2Image = new ImageIcon(dice2Path);
 
-
-        full.add(image, BorderLayout.CENTER);
-        full.add(totalDiceValue, BorderLayout.PAGE_END);
+        this.removeAll();
+        JLabel dice1 = new JLabel(dice1Image);
+        this.add(dice1, BorderLayout.WEST);
+        JLabel dice2 = new JLabel(dice2Image);
+        this.add(dice2, BorderLayout.EAST);
     }
 
 }
