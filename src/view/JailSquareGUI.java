57ae075296d3eec387671563155326c660d522ea
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2021-11-18 22:29:45 -0500

    Update JailSquareGUI.java

--- src/view/JailSquareGUI.java
+++ src/view/JailSquareGUI.java
@@ -13,31 +13,45 @@ public class JailSquareGUI extends SquareGUI {
 
     public JailSquareGUI() {
         setLayout(new BorderLayout());
-        setPreferredSize(new Dimension(100, 100));
-        this.setBorder(BorderFactory.createLineBorder(Color.black,2));
-
-        colorTag = new JPanel();
-        colorTag.setPreferredSize(new Dimension(100, 25));
-        colorTag.setBackground(Color.BLACK);
+        setPreferredSize(new Dimension(DiceGUI.SIZE, DiceGUI.SIZE));
+        this.setBorder( BorderFactory.createLineBorder(Color.black, 2));
+        this.colorTag = new JPanel();
+        this.colorTag.setPreferredSize(new Dimension(DiceGUI.SIZE, 23));
+        this.colorTag.setBackground(Color.BLACK);
         this.add(colorTag, BorderLayout.PAGE_START);
-
         jailTag = new JPanel();
-        jailTag.setPreferredSize(new Dimension(40, 25));
+        jailTag.setPreferredSize(new Dimension(36, 23));
         jailTag.setBackground(Color.RED);
         this.add(jailTag, BorderLayout.PAGE_END);
     }
 
-    public void addPlayer(String name) {
-        PlayerGUI player = new PlayerGUI(name);
-        this.colorTag.add(player);
-    }
+    /**
+     * in gui add player to jail
+     */
     public void addPlayerToJail(String name) {
         PlayerGUI player = new PlayerGUI(name);
         this.jailTag.add(player);
+        this.revalidate();
+        this.repaint();
     }
 
+    /**
+     * in gui add player to the square
+     */
+    @Override
+    public void addPlayer(String name) {
+        PlayerGUI player = new PlayerGUI(name);
+        this.colorTag.add(player);
+        this.revalidate();
+        this.repaint();
+    }
+
+    /**
+     * in gui remove player from the square
+     */
+    @Override
     public void removePlayer(String name, boolean inJail) {
-        Component[] components = new Component[0];
+        Component[] components;
         if(inJail){
             components = this.jailTag.getComponents();
         }
@@ -50,7 +64,11 @@ public class JailSquareGUI extends SquareGUI {
             JLabel player = (JLabel) label;
             if (player.getText().equals(name)) {
                 player.setVisible(false);
-                this.colorTag.remove(label);
+                if (inJail) {
+                    this.jailTag.remove(label);
+                } else {
+                    this.colorTag.remove(label);
+                }
                 this.revalidate();
                 this.repaint();
             }
@@ -65,7 +83,7 @@ public class JailSquareGUI extends SquareGUI {
         try {
 
             img = ImageIO.read(new File("src/images/jailSquare.png"));
-            resizedImage = img.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+            resizedImage = img.getScaledInstance(DiceGUI.SIZE, DiceGUI.SIZE, Image.SCALE_SMOOTH);
 
         } catch (IOException e) {
             e.printStackTrace();
