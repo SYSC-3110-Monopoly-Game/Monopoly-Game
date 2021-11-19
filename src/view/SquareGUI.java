2a9b8ff9ca37eae91b1a32ce50dedab39b780c1b
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2021-11-19 00:10:12 -0500

    Update SquareGUI.java

--- src/view/SquareGUI.java
+++ src/view/SquareGUI.java
@@ -3,7 +3,7 @@ package view;
 import javax.swing.*;
 
 public abstract class SquareGUI extends JPanel {
-    public abstract void addPlayer(String name);
 
+    public abstract void addPlayer(String name);
     public abstract void removePlayer(String name, boolean inJail);
 }
