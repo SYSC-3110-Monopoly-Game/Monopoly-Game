f9d3c7b16cab9c3173a19d548b6b5940afb90390
Shizhong-Shang <72275111+Shizhong-Shang@users.noreply.github.com>
2021-11-18 22:21:44 -0500

    Update MonopolyBoardTest.java

--- src/tests/MonopolyBoardTest.java
+++ src/tests/MonopolyBoardTest.java
@@ -1,30 +1,34 @@
 package tests;
 
+import Model.MonopolyBoard;
+import Model.Square;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MonopolyBoardTest {
+
+    private MonopolyBoard board;
 
     @BeforeEach
     void setUp() {
+        board = new MonopolyBoard();
     }
 
     @AfterEach
     void tearDown() {
-    }
-
-    @Test
-    void getNextSquare() {
+        board = null;
     }
 
     @Test
     void startingSquare() {
+        Assertions.assertEquals(board.getSquares()[0], board.startingSquare());
     }
 
     @Test
-    void displayBoard() {
+    void getNextSquare() {
+        Square next = board.getNextSquare(board.startingSquare(), 3);
+        Assertions.assertEquals("Income Tax", next.getName());
     }
 }
